49770663cf187b88fa0e7b7ede4d006641681218
Krasena Markova <kraschoo@abv.bg>
2026-06-21 13:59:52 +0300

    Add dashboard feature (squashed)

---
+++ dashboard.js
@@ -0,0 +1,11 @@
+function renderDashboard() {
+    console.log("Dashboard v1");
+}
+
+function renderWidgets() {
+    console.log("Widgets v1");
+}
+
+function initDashboard() {
+    console.log("Dashboard initialized");
+}
